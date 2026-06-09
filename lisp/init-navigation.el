7b18f6f5f7f5a6f8f012e3efdc69969b03e1d75a
jdirkse <jdirkse@mailme.not>
2026-06-09 08:50:15 +0200

    disable repeat-map, enable meow

--- lisp/init-navigation.el
+++ lisp/init-navigation.el
@@ -78,59 +78,150 @@
   ;; Automatically enable preview in embark collect buffers
   (embark-collect-mode . consult-preview-at-point-mode))
 
-;; ----------------------------
-;; Repeat map
-;; ----------------------------
+;; ;; ----------------------------
+;; ;; Repeat map
+;; ;; ----------------------------
 
-(defvar my-basic-repeat-map
-  (let ((map (make-sparse-keymap)))
-    ;; Movement
-    (define-key map (kbd "f") #'forward-char)
-    (define-key map (kbd "g") #'forward-word)
-    (define-key map (kbd "b") #'backward-char)
-    (define-key map (kbd "v") #'backward-word)
-    (define-key map (kbd "p") #'previous-line)
-    (define-key map (kbd "n") #'next-line)
-    (define-key map (kbd "a") #'move-beginning-of-line)
-    (define-key map (kbd "e") #'move-end-of-line)
+;; (defvar my-basic-repeat-map
+;;   (let ((map (make-sparse-keymap)))
+;;     ;; Movement
+;;     (define-key map (kbd "f") #'forward-char)
+;;     (define-key map (kbd "g") #'forward-word)
+;;     (define-key map (kbd "b") #'backward-char)
+;;     (define-key map (kbd "v") #'backward-word)
+;;     (define-key map (kbd "p") #'previous-line)
+;;     (define-key map (kbd "n") #'next-line)
+;;     (define-key map (kbd "a") #'move-beginning-of-line)
+;;     (define-key map (kbd "e") #'move-end-of-line)
 
-    ;; Editing
-    (define-key map (kbd "d") #'delete-char)
-    (define-key map (kbd "t") #'transpose-chars)
-    (define-key map (kbd "k") #'kill-line)
-    (define-key map (kbd "w") #'kill-region)
-    (define-key map (kbd "c") #'kill-ring-save)
-    (define-key map (kbd "SPC") #'set-mark-command)
-    (define-key map (kbd "y") #'yank)
-    (define-key map (kbd "/") #'undo)
+;;     ;; Editing
+;;     (define-key map (kbd "d") #'delete-char)
+;;     (define-key map (kbd "t") #'transpose-chars)
+;;     (define-key map (kbd "k") #'kill-line)
+;;     (define-key map (kbd "w") #'kill-region)
+;;     (define-key map (kbd "c") #'kill-ring-save)
+;;     (define-key map (kbd "SPC") #'set-mark-command)
+;;     (define-key map (kbd "y") #'yank)
+;;     (define-key map (kbd "/") #'undo)
 
-    (define-key map (kbd "1") #'digit-argument)
-    (define-key map (kbd "2") #'digit-argument)
-    (define-key map (kbd "3") #'digit-argument)
-    (define-key map (kbd "4") #'digit-argument)
-    (define-key map (kbd "5") #'digit-argument)
-    (define-key map (kbd "6") #'digit-argument)
-    (define-key map (kbd "7") #'digit-argument)
-    (define-key map (kbd "8") #'digit-argument)
-    (define-key map (kbd "9") #'digit-argument)
-    (define-key map (kbd "0") #'digit-argument)
-    map))
+;;     (define-key map (kbd "1") #'digit-argument)
+;;     (define-key map (kbd "2") #'digit-argument)
+;;     (define-key map (kbd "3") #'digit-argument)
+;;     (define-key map (kbd "4") #'digit-argument)
+;;     (define-key map (kbd "5") #'digit-argument)
+;;     (define-key map (kbd "6") #'digit-argument)
+;;     (define-key map (kbd "7") #'digit-argument)
+;;     (define-key map (kbd "8") #'digit-argument)
+;;     (define-key map (kbd "9") #'digit-argument)
+;;     (define-key map (kbd "0") #'digit-argument)
+;;     map))
 
-(dolist (cmd '(forward-char
-               forward-word
-               backward-char
-               backward-word
-               next-line
-               previous-line
-               kill-line
-               kill-ring-save
-               set-mark-command
-               undo
-               move-beginning-of-line
-               move-end-of-line
-               delete-char
-               yank))
-  (put cmd 'repeat-map 'my-basic-repeat-map))
+;; (dolist (cmd '(forward-char
+;;                forward-word
+;;                backward-char
+;;                backward-word
+;;                next-line
+;;                previous-line
+;;                kill-line
+;;                kill-ring-save
+;;                set-mark-command
+;;                undo
+;;                move-beginning-of-line
+;;                move-end-of-line
+;;                delete-char
+;;                yank))
+;;   (put cmd 'repeat-map 'my-basic-repeat-map))
+
+;; ----------------------------------------------------------------------------
+;; Meow - Modal editing
+;; ----------------------------------------------------------------------------
+
+(defun meow-setup ()
+  (setq meow-cheatsheet-layout meow-cheatsheet-layout-qwerty)
+  (meow-motion-define-key
+   '("j" . meow-next)
+   '("k" . meow-prev)
+   '("<escape>" . ignore))
+  (meow-leader-define-key
+   ;; Use SPC (0-9) for digit arguments.
+   '("1" . meow-digit-argument)
+   '("2" . meow-digit-argument)
+   '("3" . meow-digit-argument)
+   '("4" . meow-digit-argument)
+   '("5" . meow-digit-argument)
+   '("6" . meow-digit-argument)
+   '("7" . meow-digit-argument)
+   '("8" . meow-digit-argument)
+   '("9" . meow-digit-argument)
+   '("0" . meow-digit-argument)
+   '("/" . meow-keypad-describe-key)
+   '("?" . meow-cheatsheet))
+  (meow-normal-define-key
+   '("0" . meow-expand-0)
+   '("9" . meow-expand-9)
+   '("8" . meow-expand-8)
+   '("7" . meow-expand-7)
+   '("6" . meow-expand-6)
+   '("5" . meow-expand-5)
+   '("4" . meow-expand-4)
+   '("3" . meow-expand-3)
+   '("2" . meow-expand-2)
+   '("1" . meow-expand-1)
+   '("-" . negative-argument)
+   '(";" . meow-reverse)
+   '("," . meow-inner-of-thing)
+   '("." . meow-bounds-of-thing)
+   '("[" . meow-beginning-of-thing)
+   '("]" . meow-end-of-thing)
+   '("a" . meow-append)
+   '("A" . meow-open-below)
+   '("b" . meow-back-word)
+   '("B" . meow-back-symbol)
+   '("c" . meow-change)
+   '("d" . meow-delete)
+   '("D" . meow-backward-delete)
+   '("e" . meow-next-word)
+   '("E" . meow-next-symbol)
+   '("f" . meow-find)
+   '("g" . meow-cancel-selection)
+   '("G" . meow-grab)
+   '("h" . meow-left)
+   '("H" . meow-left-expand)
+   '("i" . meow-insert)
+   '("I" . meow-open-above)
+   '("j" . meow-next)
+   '("J" . meow-next-expand)
+   '("k" . meow-prev)
+   '("K" . meow-prev-expand)
+   '("l" . meow-right)
+   '("L" . meow-right-expand)
+   '("m" . meow-join)
+   '("n" . meow-search)
+   '("o" . meow-block)
+   '("O" . meow-to-block)
+   '("p" . meow-yank)
+   '("q" . meow-quit)
+   '("Q" . meow-goto-line)
+   '("r" . meow-replace)
+   '("R" . meow-swap-grab)
+   '("s" . meow-kill)
+   '("t" . meow-till)
+   '("u" . meow-undo)
+   '("U" . meow-undo-in-selection)
+   '("v" . meow-visit)
+   '("w" . meow-mark-word)
+   '("W" . meow-mark-symbol)
+   '("x" . meow-line)
+   '("X" . meow-goto-line)
+   '("y" . meow-save)
+   '("Y" . meow-sync-grab)
+   '("z" . meow-pop-selection)
+   '("'" . repeat)
+   '("<escape>" . ignore)))
+
+(require 'meow)
+(meow-setup)
+(meow-global-mode 1)
 
 ;;; Provide feature
 (provide 'init-navigation)
